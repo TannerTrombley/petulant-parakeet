4344eabdea5299aa5e5e949858dabdfd9cb8abfb
Lucas Katt <kattlucas1@gmail.com>
2015-09-12 14:00:26 -0400

    started integration with bluetooth chat example

--- android/SmartBike/app/src/main/java/org/petulant/smartbike/MainActivity.java
+++ android/SmartBike/app/src/main/java/org/petulant/smartbike/MainActivity.java
@@ -1,25 +1,34 @@
 package org.petulant.smartbike;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            BluetoothChatFragment fragment = new BluetoothChatFragment();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
     }
+
 
     public void openMap(View view) {
         Intent intent = new Intent(this, MapActivity.class);
         startActivity(intent);
     }
+
 
     /*
     @Override
