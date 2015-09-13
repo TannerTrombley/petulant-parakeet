3ffad495fb8ddabe2ed23798aeb952085e4821dd
johndo <johndoh@umich.edu>
2015-09-13 05:25:40 -0400

    done with maps

--- android/SmartBike/app/src/main/java/org/petulant/smartbike/MapsActivity.java
+++ android/SmartBike/app/src/main/java/org/petulant/smartbike/MapsActivity.java
@@ -1,8 +1,19 @@
 package org.petulant.smartbike;
 
+import android.content.Intent;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -14,6 +25,8 @@ public class MapsActivity extends FragmentActivity {
     private GoogleMap mMap; // Might be null if Google Play services APK is not available
     private static LatLng Destination = null;
     private Marker destination = null;
+    private Marker crntlctnMarker = null;
+    private static LatLng CrntLctn = null;
 
 
     @Override
@@ -57,6 +70,20 @@ public class MapsActivity extends FragmentActivity {
         }
     }
 
+
+
+    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+        @Override
+        public void onMyLocationChange(Location location) {
+            LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+            crntlctnMarker = mMap.addMarker(new MarkerOptions().position(loc));
+            CrntLctn = loc;
+            if(mMap != null){
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+            }
+        }
+    };
+
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
@@ -68,6 +95,7 @@ public class MapsActivity extends FragmentActivity {
         mMap.getUiSettings().setTiltGesturesEnabled(true);
         mMap.getUiSettings().setCompassEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setOnMyLocationChangeListener(myLocationChangeListener);
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng point){
@@ -84,4 +112,39 @@ public class MapsActivity extends FragmentActivity {
             destination = mMap.addMarker(new MarkerOptions().position(new LatLng(Destination.latitude, Destination.longitude)).title("Destination"));
         }
     }
+
+    public String resp;
+
+
+
+    public void GetDirections (View strtBtn ){
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://maps.googleapis.com/maps/api/directions/json?origin="+CrntLctn.latitude+","+CrntLctn.longitude+"&destination="+Destination.latitude+","+Destination.longitude+"&mode=bicycling&key=AIzaSyCnf3zwTQ1-PEElMQgKdUGJ14OqJpyHq3Q";
+        Log.i("url", url);
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        resp = response;
+                        Log.i("name", resp);
+                        Intent intent = new Intent(getBaseContext(), SensorActivity.class);
+                        intent.putExtra("DIRECTIONS", resp);
+                        startActivity(intent);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+    }
+
+
 }
